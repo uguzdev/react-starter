403be39c951e432db26bdfc94f5f8e00c01da7c9
Furkan Uğuz <oguzfurkanss@gmail.com>
2022-08-23 14:41:05 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/function-component-definition': 'off',
+    'prettier/prettier': ['error', { singleQuote: true }],
+  },
+};
